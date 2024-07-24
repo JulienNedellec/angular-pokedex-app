9b9f774d59e70d7fd6e3c2a718d143e355e408b6
JulienNedellec <nedellec.julien.78@gmail.com>
2024-07-24 12:34:52 +0200

    suppression d'un import inutile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { POKEMON_LIST } from './pokemon-list.fake';
 import { Pokemon } from './pokemon.model';
