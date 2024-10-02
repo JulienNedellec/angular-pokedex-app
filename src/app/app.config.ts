50072022e7c0278a8922135e994b2beb20bb57d8
JulienNedellec <nedellec.julien.78@gmail.com>
2024-10-02 14:47:44 +0200

    Refactor imports in app.component.ts and add DatePipe Add new routes for Pokémon editing and page not found Implement page not found component Implement Pokémon edit component Update Pokémon profile component to include edit button Add Pokémon edit component template Add Pokémon edit component TypeScript file Update Pokémon model to include getPokemonColor function Update Pokémon profile component template to include edit button Add Pokémon edit component template Add Pokémon edit component TypeScript file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,24 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { Routes, provideRouter } from '@angular/router'; // 👈
 import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component'; // 👈
 import { PokemonProfileComponent } from './pokemon/pokemon-profile/pokemon-profile.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PokemonEditComponent } from './pokemon/pokemon-edit/pokemon-edit.component';
 
 // 👇
 const routes: Routes = [
-  { path: 'pokemons/:id', component: PokemonProfileComponent },
-  { path: 'pokemons', component: PokemonListComponent },
+  {
+    path: 'pokemons/edit/:id',
+    component: PokemonEditComponent,
+    title: 'Pokémon',
+  },
+  {
+    path: 'pokemons/:id',
+    component: PokemonProfileComponent,
+    title: 'Pokémon',
+  },
+  { path: 'pokemons', component: PokemonListComponent, title: 'Pokédex' },
   { path: '', redirectTo: '/pokemons', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent, title: 'Page introuvable' },
 ];
 
 export const appConfig: ApplicationConfig = {
